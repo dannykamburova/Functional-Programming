2cfab3f2369f07ed3669dad2206a7079333fc247
Yordanka Kamburova <104228334+dannykamburova@users.noreply.github.com>
2023-03-22 16:52:47 +0200

    Add files via upload

---
+++ ForHomeWeek01/ForHomeWeek01Task8.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+main = do
+    print $ snail 3 2 1 == 2
+    print $ snail 10 3 1 == 5
+    print $ snail 10 3 2 == 8
+    print $ snail 100 20 5 == 7
+    print $ snail 5 10 3 == 1
+
+snail :: Int -> Int -> Int -> Int
+snail x y z = 
