f6c31a7120d2c6daff30bc01624fa3e7c5f15b0f
Yordanka Kamburova <104228334+dannykamburova@users.noreply.github.com>
2023-03-23 22:22:02 +0200

    Update ForHomeWeek01Task04.hs

--- ForHomeWeek01/ForHomeWeek01Task04.hs
+++ ForHomeWeek01/ForHomeWeek01Task04.hs
@@ -3,14 +3,17 @@ main = do
     print $ myGcdG 5 13 == 1
     print $ myGcdG 13 1235 == 13
 
-    --print $ myGcdPM 5 13 == 1
-    --print $ myGcdPM 13 1235 == 13
+    print $ myGcdPM 5 13 == 1
+    print $ myGcdPM 13 1235 == 13
  
 myGcdG :: Int -> Int -> Int
 myGcdG x y 
  | x == 0 = y 
  | y == 0 = x
- | otherwise = myGcdG x (mod x y) 
+ | otherwise = myGcdG y (mod x y) 
 
---myGcdPM :: Int -> Int -> Int
---myGcdPM x y = 
+myGcdPM :: Int -> Int -> Int
+myGcdPM x y = helper x y
+    where
+    helper x 0 = x
+    helper y (mod x y)
