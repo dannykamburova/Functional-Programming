16e09943d821f44c7fb84d106a7dedaa233acac4
Yordanka Kamburova <104228334+dannykamburova@users.noreply.github.com>
2023-03-23 10:07:10 +0200

    Update ForHomeWeek01Task02.hs

--- ForHomeWeek01/ForHomeWeek01Task02.hs
+++ ForHomeWeek01/ForHomeWeek01Task02.hs
@@ -7,7 +7,7 @@ main = do
     print $ sumCubesNoPow 10 50 == 126000
 
 sumCubesPow :: Int -> Int -> Int
-sumCubesPow x y = (x ^ 3) + (y ^ 3)
+sumCubesPow x y = x^3 + y^3
 
 sumCubesNoPow :: Int -> Int -> Int
-sumCubesNoPow x y = (x * x * x) + (y * y * y)
+sumCubesNoPow x y = x * x * x + y * y * y
